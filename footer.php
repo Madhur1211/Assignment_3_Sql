a528d9ca2a37ae00300b5e44a0777f582e3c3d8a
Madhur1211 <115094317+Madhur1211@users.noreply.github.com>
2022-11-25 16:34:39 -0500

    Update footer.php

--- footer.php
+++ footer.php
@@ -8,6 +8,6 @@
 <footer class="bg-light text-center text-lg-start">
     <div class="text-center p-3" style="background-color: #fff;">
         © 2022 Copyright:
-        <a class="text-dark" href="https://www.tutorialrepublic.com/php-tutorial/php-mysql-crud-application.php">Learn More!!!</a>
+        <a class="text-dark" href="https://www.tutorialrepublic.com/php-tutorial/php-mysql-crud-application.php">Learn More!!!</a> <!--link other site which I used for reference-->
     </div>
 </footer>
